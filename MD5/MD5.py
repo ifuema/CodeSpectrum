3f1170220aebd2b39ff0bc823519019049ae3bf9
ifuema <75777852+ifuema@users.noreply.github.com>
2024-12-17 14:48:47 +0800

    Update MD5.py

--- MD5/MD5.py
+++ MD5/MD5.py
@@ -2,21 +2,25 @@ import os
 import hashlib
 from tqdm import tqdm
 
-def calculate_md5(file_path):
-    md5 = hashlib.md5()
+def calculate_hash(file_path, hash_algorithms):
     total_size = os.path.getsize(file_path)
     progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
 
+    # 初始化哈希算法字典
+    hashes = {name: hashlib.new(name) for name in hash_algorithms}
+
     with open(file_path, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
-            md5.update(chunk)
+            for hash_name, hash_obj in hashes.items():
+                hash_obj.update(chunk)
             progress_bar.update(len(chunk))
+    
     progress_bar.close()
 
-    return md5.hexdigest()
+    # 返回计算后的哈希值
+    return {name: hash_obj.hexdigest() for name, hash_obj in hashes.items()}
 
-def calculate_md5_for_folder(folder_path):
-    md5 = hashlib.md5()
+def calculate_hash_for_folder(folder_path, hash_algorithms):
     total_size = 0
     for root, dirs, files in os.walk(folder_path):
         for file in files:
@@ -25,26 +29,36 @@ def calculate_md5_for_folder(folder_path):
 
     progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
     
+    # 初始化哈希算法字典
+    hashes = {name: hashlib.new(name) for name in hash_algorithms}
+
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             file_path = os.path.join(root, file)
             with open(file_path, "rb") as f:
                 for chunk in iter(lambda: f.read(4096), b""):
-                    md5.update(chunk)
+                    for hash_name, hash_obj in hashes.items():
+                        hash_obj.update(chunk)
                     progress_bar.update(len(chunk))
+    
     progress_bar.close()
 
-    return md5.hexdigest()
+    # 返回计算后的哈希值
+    return {name: hash_obj.hexdigest() for name, hash_obj in hashes.items()}
 
 def main():
     while True:
         path = input("请输入文件或文件夹地址：")
         if os.path.isfile(path):
-            file_md5 = calculate_md5(path)
-            print("文件的MD5值:", file_md5)
+            print(f"计算文件 {path} 的哈希值：")
+            hash_values = calculate_hash(path, ["md5", "sha256"])
+            print("MD5值:", hash_values["md5"])
+            print("SHA-256值:", hash_values["sha256"])
         elif os.path.isdir(path):
-            folder_md5 = calculate_md5_for_folder(path)
-            print("文件夹的MD5值:", folder_md5)
+            print(f"计算文件夹 {path} 的哈希值：")
+            hash_values = calculate_hash_for_folder(path, ["md5", "sha256"])
+            print("MD5值:", hash_values["md5"])
+            print("SHA-256值:", hash_values["sha256"])
         else:
             print("输入的不是有效的文件或文件夹地址。")
 
